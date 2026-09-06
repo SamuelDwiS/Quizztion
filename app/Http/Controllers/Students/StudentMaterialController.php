2e60499efaef51bb0f035b464a5276fc58c72199
SamuelDwiS <samdwisaputro02@gmail.com>
2026-09-06 22:19:08 +0700

    feat: refactor StudentMaterialController; add constructor and show method for material retrieval

--- app/Http/Controllers/Students/StudentMaterialController.php
+++ app/Http/Controllers/Students/StudentMaterialController.php
@@ -3,12 +3,25 @@
 namespace App\Http\Controllers\Students;
 
 use App\Http\Controllers\Controller;
+use App\Models\Material;
+use App\Providers\StudentService;
 use Illuminate\Http\Request;
 
 class StudentMaterialController extends Controller
 {
-    public function show()
+
+    protected $studentService;
+    protected $studentMaterialService;
+
+    public function __construct(StudentService $studentService)
     {
-        
+        $this->studentService = $studentService;
     }
+
+    public function show($id)
+    {
+        $material = Material::with('subject')->findOrFail($id);
+        
+    }    
+
 }
