72e7f861d3404ffd6a7ef1bb5a03cfa7abde5c6a
SamuelDwiS <samdwisaputro02@gmail.com>
2026-09-06 23:46:51 +0700

    feat: implement StudentMaterialService and StudentProfileService methods; update providers to include StudentService

--- app/Services/Student/StudentMaterialService.php
+++ app/Services/Student/StudentMaterialService.php
@@ -2,6 +2,10 @@
 
 namespace App\Services\Student;
 
+use App\Models\Material;
+use App\Models\User;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+
 class StudentMaterialService
 {
     /**
@@ -11,4 +15,25 @@ class StudentMaterialService
     {
         //
     }
+
+    public function getMaterials() {}
+    public function getMaterialDetail() {}
+    public function updateProgress(){}
+    public function markAsComplete(){}
+    public function generateAiSummary(){}
+    // public function paginateFor(User $student, int $perPage = 9): LengthAwarePaginator
+    // {
+    //     if (!$student->studentClass()) {
+    //         return Material::query()
+    //             ->with(['subject.department', 'author'])
+    //             ->whereHas('subject', function ($query) use ($student) {
+    //                 $query->where(
+    //                     'department_id',
+    //                     $student->studentClass?->department_id
+    //                 );
+    //             })
+    //             ->latest()
+    //             ->paginate($perPage);
+    //     }
+    // }
 }
--- app/Services/Student/StudentProfileService.php
+++ app/Services/Student/StudentProfileService.php
@@ -2,13 +2,28 @@
 
 namespace App\Services\Student;
 
+use App\Models\User;
+
 class StudentProfileService
 {
     /**
      * Create a new class instance.
      */
-    public function __construct()
+    public function getProfile(User $student): User
     {
-        //
+        return $student->load([
+            'studentClass.department',
+            'roles',
+        ]);
+    }
+
+    public function updateProfile(User $student, array $data): User
+    {
+        $student->update([
+            'name' => $data['name'],
+            'student_class_id' => $data['student_class_id'] ?? $student->student_class_id,
+        ]);
+
+        return $student->refresh();
     }
 }
